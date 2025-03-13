8fe013a4d805c20c952745eb08031e62a0da832a
Khervyn Gupta <khervyngupta@gmail.com>
2025-03-14 01:22:25 +0530

    Fixed CSS issue with mobile menu

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    emailServiceID: 'service_xnlnsnp',
+    emailTemplateID: 'template_ds22czc',
+    emailPublicKey: '0xeLApnxtQl8YMqKS'
+  };
+  
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  emailServiceID: 'service_xnlnsnp',
+  emailTemplateID: 'template_ds22czc',
+  emailPublicKey: '0xeLApnxtQl8YMqKS'
+};
